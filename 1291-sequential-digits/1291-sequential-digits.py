46f2cf88c892fbc73615b6489703261cbbe3b0c9
Shahid Mansuri <140784065+Shahid09092005@users.noreply.github.com>
2026-07-13 12:28:15 +0530

    Time: 0 ms (100%), Space: 19.2 MB (64.31%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -4,7 +4,8 @@ class Solution:
         s = "123456789"
 
         for i in range(len(s)):
-            for j in range(i + 1, len(s) + 1):
+            for j in range(i+1, len(s) + 1):
+                # s[0:0] give an empty string
                 num = int(s[i:j])
 
                 if low <= num <= high:
