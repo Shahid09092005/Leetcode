1a4718e477fe42a2cd9dba315cd9cc644318a4af
Shahid Mansuri <140784065+Shahid09092005@users.noreply.github.com>
2026-07-22 13:17:14 +0530

    Time: 7 ms (58.63%), Space: 20.9 MB (10.85%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,25 +3,47 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rSet= set()
-        cSet = set()
+        isFirstColContainsZero = False
+        isFirstRowContainsZero = False
         rows = len(matrix)
         cols = len(matrix[0])
-        for i in range(0,rows):
-            for j in range(0,cols):
-                if(matrix[i][j]==0):
-                    rSet.add(i)
-                    cSet.add(j)
-            # ourt of inner loop
-        # out of outer loop
-        # convert rows to zeros
-        for r in rSet:
-            for j in range(0,cols):
-                matrix[r][j]=0
-        # convert cols to zeros
-        for c in cSet:
-            for i in range(0,rows):
-                matrix[i][c]=0
-        # return modify matrix
+        # checking in first rows
+        for c in range(0,cols):
+            if(matrix[0][c]==0):
+                isFirstRowContainsZero=True
+                break
+        # checking in first cols
+        for r in range(0,rows):
+            if(matrix[r][0]==0):
+                isFirstColContainsZero=True
+                break
+        # now using first row and cols for referecne that contains zero
+        for r in range(1,rows):
+            for c in range(1,cols):
+                if(matrix[r][c]==0):
+                    # for firstRow
+                    matrix[0][c]=0
+                    # for firstCol
+                    matrix[r][0]=0
+        # outter loop ends here
+        # matrix 0 modifications
+        for r in range(1,rows):
+            for c in range(1,cols):
+                if(matrix[0][c]==0 or matrix[r][0]==0):
+                    matrix[r][c]=0
+        # outter loop ends here
+        # matrix 0 modifications
+        # now changes in first rows
+        if(isFirstRowContainsZero):
+            for c in range(0,cols):
+                matrix[0][c]=0
+
+        # for first cols
+        if(isFirstColContainsZero):
+            for r in range(0,rows):
+                matrix[r][0]=0
+        # return ans
         return matrix
-        
+
+
+
