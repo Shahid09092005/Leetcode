db25888a3d2dacb5aae2a7155fc67811b64d86e7
Shahid Mansuri <140784065+Shahid09092005@users.noreply.github.com>
2026-07-29 14:44:05 +0530

    Time: 4 ms (44.78%), Space: 19.3 MB (77.86%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -10,36 +10,22 @@ class Solution:
         ansh= ListNode(-100)
         anstemp=ansh
         carry=0
-        while(tempA is not None and tempB is not None):
-            addTwoNum = tempA.val+tempB.val+carry
+        addTwoNum=0
+        while((tempA is not None) or (tempB is not None)):
+            addTwoNum=carry
+            if(tempA is not None):
+                addTwoNum += tempA.val
+                tempA = tempA.next
+            if(tempB is not None):
+                addTwoNum += tempB.val
+                tempB = tempB.next
             hereVal = addTwoNum%10
             carry = (int)(addTwoNum//10)
             anstemp.next = ListNode(hereVal)
+
             # move to next
             anstemp = anstemp.next
-            # move A and B
-            tempA = tempA.next
-            tempB = tempB.next
-        if((tempA is None )and (tempB is not None)):# temp A ends 
-            while(tempB is not None):
-                addTwoNum = tempB.val+carry
-                hereVal = addTwoNum%10
-                carry = (int)(addTwoNum//10)
-                anstemp.next = ListNode(hereVal)
-                # move to next
-                anstemp = anstemp.next
-                # move B
-                tempB = tempB.next
-        if((tempA is not None )and (tempB is None)):# temp B ends 
-            while(tempA is not None):
-                addTwoNum = tempA.val+carry
-                hereVal = addTwoNum%10
-                carry = (int)(addTwoNum//10)
-                anstemp.next = ListNode(hereVal)
-                # move to next
-                anstemp = anstemp.next
-                # move A
-                tempA = tempA.next
+
         if(carry==1):
             anstemp.next = ListNode(1)
             anstemp = anstemp.next
