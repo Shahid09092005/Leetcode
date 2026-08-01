b97f2e08043c763ff9ff899de54dfce2747ea87a
Shahid Mansuri <140784065+Shahid09092005@users.noreply.github.com>
2026-08-01 08:44:34 +0530

    Time: 0 ms (100%), Space: 19.5 MB (42.77%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,15 +1,21 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        def solve(i,j):
+        def solve(i,j,dp):
             if(i>j):
                 return 0
             if(i==j):
                 return nums[i]
-            choose_i = nums[i]+min(solve(i+2,j),solve(i+1,j-1))
-            choose_j = nums[j]+min(solve(i+1,j-1),solve(i,j-2))
-            return max(choose_i,choose_j)
-
-        pl1=solve(0,len(nums)-1)
+            if(arr[i][j]!=-1):
+                return dp[i][j]
+            
+            choose_i = nums[i]+min(solve(i+2,j,dp),solve(i+1,j-1,dp))
+            choose_j = nums[j]+min(solve(i+1,j-1,dp),solve(i,j-2,dp))
+            dp[i][j] = max(choose_i,choose_j)
+            return dp[i][j]
+        # momoization
+        n = len(nums)
+        arr = [[-1]*n for _ in range(n)]
+        pl1=solve(0,n-1,arr)
         totalScore = sum(nums)
         pl2 = totalScore-pl1
         if(pl1>=pl2):
