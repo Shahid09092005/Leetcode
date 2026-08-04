da6327be485a50af0a693acfdd9b125da67df276
Shahid Mansuri <140784065+Shahid09092005@users.noreply.github.com>
2026-08-04 09:05:07 +0530

    Time: 2 ms (75.11%), Space: 19.2 MB (55.74%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -1,10 +1,12 @@
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
         arr = [0]*101
+        maxi = float('-inf')
+        mini = float('inf')
         for x in nums:
             arr[x]+=1
-        mini = min(nums)
-        maxi = max(nums)
+            maxi = max(maxi,x)
+            mini = min(mini,x)
         lst=[]
         for x in range(mini,maxi+1):
             if arr[x]==0:
