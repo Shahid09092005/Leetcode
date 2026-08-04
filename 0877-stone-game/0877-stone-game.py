aac7e1bfbc3ca90e3192a6d4fbc899e9fc6bf9f4
Shahid Mansuri <140784065+Shahid09092005@users.noreply.github.com>
2026-08-04 08:07:48 +0530

    Time: 257 ms (26.82%), Space: 43 MB (26.86%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,3 +1,24 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        def solve(i,j,dp):
+            if(i>j):
+                return 0
+            if(i==j):
+                return piles[i]
+            if(arr[i][j]!=-1):
+                return dp[i][j]
+            
+            choose_i = piles[i]+min(solve(i+2,j,dp),solve(i+1,j-1,dp))
+            choose_j = piles[j]+min(solve(i+1,j-1,dp),solve(i,j-2,dp))
+            dp[i][j] = max(choose_i,choose_j)
+            return dp[i][j]
+        # momoization
+        n = len(piles)
+        arr = [[-1]*n for _ in range(n)]
+        pl1=solve(0,n-1,arr)
+        totalScore = sum(piles)
+        pl2 = totalScore-pl1
+        if(pl1>=pl2):
+            return True
+        return False
+        
